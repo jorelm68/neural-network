8edfd4eb689d988d63d7800ba15f8028f9dfce1f
jorelm68 <methan@umich.edu>
2024-11-12 16:38:49 -0500

    setup main for taking in arguments

--- main.cpp
+++ main.cpp
@@ -1,17 +1,48 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <algorithm>
-#include <cmath>
-#include <map>
-#include <set>
-#include <queue>
-#include <stack>
 
 using namespace std;
 
-// Main function taking in arguments
-int main(char **argv, int argc) {
-    // Code here
+// Main class ------------------------------------------------
+class Main {
+    public:
+        Main(int argc, char **argv);
+        ~Main();
+        void printArguments();
+
+    private:
+        int argc;
+        char **argv;
+};
+// -----------------------------------------------------------
+
+// Main class constructor and destructor ---------------------
+Main::Main(int argc, char **argv) {
+    cout << "Main object created" << endl;
+
+    // Store the arguments
+    this->argc = argc;
+    this->argv = argv;
+}
+Main::~Main() {
+    cout << "Main object destroyed" << endl;
+}
+// -----------------------------------------------------------
+
+// Main class member functions -------------------------------
+void Main::printArguments() {
+    for (int i = 0; i < this->argc; ++i) {
+        cout << "Argument " << i << ": " << this->argv[i] << endl;
+    }
+}
+// -----------------------------------------------------------
+
+// Main function taking in arguments -------------------------
+int main(int argc, char **argv) {
+    Main main(argc, argv);
+
+    main.printArguments();
     return 0;
 }
+// -----------------------------------------------------------
